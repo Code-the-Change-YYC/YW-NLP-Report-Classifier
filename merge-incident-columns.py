ab88b19e8a9a4878533c42de2f867dfe4db3755b
DhyeyLalseta <dhyeylalseta@outlook.com>
2020-05-04 16:14:48 -0600

    Incident columns moved to end

--- merge-incident-columns.py
+++ merge-incident-columns.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[66]:
+# In[1]:
 
 
 import pandas as pd
 
 
-# In[67]:
+# In[2]:
 
 
 filepath = 'scrubber/data_scrubbed.csv'
@@ -16,7 +16,16 @@ pd.set_option('display.max_columns', None)
 df['INCIDENT_TYPE'] = df['INCIDENT_1_OLD'].fillna('') + df['INCIDENT_TYPE_1'].fillna('')
 
 
-# In[68]:
+# In[3]:
+
+
+df = df[[col for col in df if col not in ['INCIDENT_TYPE_1', 'INCIDENT_TYPE_2', 'INCIDENT_1_OLD', 'INCIDENT_TYPE_OTHER']] 
+        + ['INCIDENT_TYPE_2', 'INCIDENT_TYPE_OTHER']]
+
+
+# Incident Types will be at the end of the csv.
+
+# In[4]:
 
 
 df.to_csv('merged-data/merged_data.csv')
