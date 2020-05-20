cf8b37c668affae1c0b7ec2aeda3af9c03e69860
Calum Sieppert <sieppertcalum@gmail.com>
2020-05-20 15:33:09 -0600

    NLP-15 Clean up NLTK visualisation script

--- scrubber/nltk_incident_type_vis.py
+++ scrubber/nltk_incident_type_vis.py
@@ -4,7 +4,17 @@ import matplotlib
 import nltk
 import pandas as pd
 
-from scrubber.data import columns
+columns = ["DATETIME_INCIDENT", "LOCATION", "LOCATION_DETAIL",
+           "CLIENT_PRIMARY", "CLIENT_SECONDARY", "CHILD_INVOLVED",
+           "NON_CLIENT_INVOLVED", "PROGRAM", "CFS", "EMS", "FIRE",
+           "MISC_SERVICE_FLAG", "DOAP_PACT", "POLICE", "MISC_SERVICE",
+           "INCIDENT_TYPE_1", "INCIDENT_TYPE_2", "INCIDENT_1_OLD",
+           "INCIDENT_TYPE_OTHER", "DESCRIPTION", "RESPONSE_CHILD_WELFARE",
+           "RESPONSE_EVAC", "RESPONSE_FIRST_AID", "RESPONSE_INFECTION_PREVENTION_PROTOCOL",
+           "RESPONSE_MENTAL_ASSESSMENT", "RESPONSE_NALOXONE",
+           "RESPONSE_PERSON_BARRED", "RESPONSE_SAFETY_ASSESSMENT",
+           "RESPONSE_SAFETY_PLANNING", "RESPONSE_OTHER", "RESPONSE_OTHER_DESC",
+           "DATETIME_WRITTEN"]
 
 report_data = pd.read_csv("data-sensitive.csv", na_filter=False)
 report_data.columns = columns
