857f454ea63cefe4281d600797773dddbad46927
Calum Sieppert <sieppertcalum@gmail.com>
2020-05-23 10:20:31 -0600

    NLP-18 Gather and test for all incident types from dropdown

--- preprocess/incident_types/incident_types_d.py
+++ preprocess/incident_types/incident_types_d.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Dict
 
 
 class IncidentType(Enum):
@@ -12,63 +13,75 @@ class IncidentType(Enum):
     OTR = "Other"
     SECUR_C = "Security concern"
     SUICIDE_C = "Suicide attempt"
+    ABU_CHI = "Suspicion/allegation of child abuse - child is not a client"
+    ABU_CLI = "Suspicion/allegation of abuse towards or against client"
+    PRI_BRE = "Suspected or actual breach of privacy"
+    MEDIA = "Media/3rd party contact"
+    INJURY = "Injury"
+    ILLEGAL = "Illegal activity on premises"
+    EXPOSURE = "Exposure"
+    COVID = "COVID-19 Confirmed"
+    MISSING = "Client missing"
+    DEATH_ONSITE = "Client death (onsite)"
+    DEATH_OFFSITE = "Client death (offsite)"
+    CHI_ABD = "Child abandonment"
 
 
 # below is the record for replacement, e.g. x:y replace x with y
-replacements = {
+replacements: Dict[str, str] = {
     # "child abandonment"
     # "Client aggression towards another person"
-    "Public disturbance": IncidentType.CLIENT_P,
-    "Noise disturbance": IncidentType.CLIENT_P,
+    "Public disturbance": IncidentType.CLIENT_P.value,
+    "Noise disturbance": IncidentType.CLIENT_P.value,
     # "Client aggression towards property"
-    "Damage to property within unit": IncidentType.CLIENT_PT,
+    "Damage to property within unit": IncidentType.CLIENT_PT.value,
     #  "Client death (off-site)"
     #  "Client death (on-site)"
     #  "Client death missing"
     # "Concern for welfare of a child"
-    "Custodial parent did not pick up child after exchange": IncidentType.CONCERN_C,
+    "Custodial parent did not pick up child after exchange": IncidentType.CONCERN_C.value,
     # "COVID-19 Confirmed"
     # "Exposure"
     # "Homicide (Threat or attempt)"
-    "Threatening with alleged weapon": IncidentType.HOM_T,
-    "Domestic violence": IncidentType.HOM_T,
+    "Threatening with alleged weapon": IncidentType.HOM_T.value,
+    "Domestic violence": IncidentType.HOM_T.value,
     # "Illegal activity on premises"
     # "Injury"
     # "Media/3rd party contact"
     # "Medical emergency"
-    "Medical": IncidentType.MED_E,
-    "Client health risk": IncidentType.MED_E,
-    "Drug overdose": IncidentType.MED_E,
-    "Health concern": IncidentType.MED_E,
-    "Health corcern": IncidentType.MED_E,
-    "High intoxication, ems call.": IncidentType.MED_E,
-    "Hospital trip": IncidentType.MED_E,
-    "Medical emergecny": IncidentType.MED_E,
-    "Medical emergency (overdose)": IncidentType.MED_E,
-    "Medical health emergency": IncidentType.MED_E,
-    "Seizure/ anxiety attack": IncidentType.MED_E,
-    "Overdose": IncidentType.MED_E,
-    "Medical Concern": IncidentType.MED_E,
-    "Medical Emergecny": IncidentType.MED_E,
-    "Medical concern": IncidentType.MED_E,
+    "Medical": IncidentType.MED_E.value,
+    "Client health risk": IncidentType.MED_E.value,
+    "Drug overdose": IncidentType.MED_E.value,
+    "Health concern": IncidentType.MED_E.value,
+    "Health corcern": IncidentType.MED_E.value,
+    "High intoxication, ems call.": IncidentType.MED_E.value,
+    "Hospital trip": IncidentType.MED_E.value,
+    "Medical emergecny": IncidentType.MED_E.value,
+    "Medical emergency (overdose)": IncidentType.MED_E.value,
+    "Medical health emergency": IncidentType.MED_E.value,
+    "Seizure/ anxiety attack": IncidentType.MED_E.value,
+    "Overdose": IncidentType.MED_E.value,
+    "Medical Concern": IncidentType.MED_E.value,
+    "Medical Emergecny": IncidentType.MED_E.value,
+    "Medical concern": IncidentType.MED_E.value,
 
     # "Mental health emergency"
-    "Potential overdose": IncidentType.MEN_HE,
-    "Excessive use of medication": IncidentType.MEN_HE,
+    "Potential overdose": IncidentType.MEN_HE.value,
+    "Excessive use of medication": IncidentType.MEN_HE.value,
 
     # "Other"
-    "Positive incident involving cps": IncidentType.OTR,
-    "Small fire on stove top": IncidentType.OTR,
-    "Fire": IncidentType.OTR,
-    "Od": IncidentType.OTR,
+    "Positive incident involving cps": IncidentType.OTR.value,
+    "Small fire on stove top": IncidentType.OTR.value,
+    "Fire": IncidentType.OTR.value,
+    "Od": IncidentType.OTR.value,
 
     # "Security concern"
-    "Removal of barred guest": IncidentType.SECUR_C,
-    "Unwanted man at door refused to leave property": IncidentType.SECUR_C,
-    "Property damage": IncidentType.SECUR_C,
+    "Removal of barred guest": IncidentType.SECUR_C.value,
+    "Unwanted man at door refused to leave property": IncidentType.SECUR_C.value,
+    "Property damage": IncidentType.SECUR_C.value,
 
     # "Suicide attempt"
-    "Client at-risk of suicide": IncidentType.SUICIDE_C,
+    "Client at-risk of suicide": IncidentType.SUICIDE_C.value,
 
     # "Suspected or actual breach of privacy"
     # "Suspicion/allegation of abuse towards or against client"
--- preprocess/incident_types/test_processor.py
+++ preprocess/incident_types/test_processor.py
@@ -1,20 +1,19 @@
 import unittest
 
-import incident_types.incident_types_d
 from incident_types import processor
+from incident_types.incident_types_d import IncidentType, replacements
 from report_data import ReportData
 
 
 class TestProcessor(unittest.TestCase):
-    # TODO: Test that only incident types from the current dropdown are in the columns after pre-processing
     def test_process(self):
         """Ensure replacements are made as expected"""
         report_df = ReportData().get_raw_report_data()
         processor.IncidentTypesProcessor().process(report_df)
         for col_name in processor.IncidentTypesProcessor.col_names:
-            for inc_type in report_df[col_name]:
-                if inc_type in incident_types.incident_types_d.replacements.keys():
-                    self.fail()
+            for inc_type in report_df[col_name].fillna(''):
+                self.assertNotIn(inc_type, replacements.keys())
+                self.assertIn(inc_type, [v.value for v in IncidentType.__members__.values()] + [''])
 
 
 if __name__ == '__main__':
