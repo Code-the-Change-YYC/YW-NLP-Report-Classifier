170693378643ce2b6fa85f7b6be1d2dc8767f92a
Calum Sieppert <sieppertcalum@gmail.com>
2020-05-22 23:12:11 -0600

    NLP-18 Packages for processor steps

--- preprocess/incident_types/test_incident_types.py
+++ preprocess/incident_types/test_incident_types.py
@@ -1,6 +1,6 @@
 import unittest
 
-import incident_types
+from incident_types import processor
 import report_data
 
 
@@ -9,10 +9,10 @@ class TestIncTypes(unittest.TestCase):
     def test_preprocess(self):
         """Ensure replacements are made as expected"""
         report_df = report_data.get_raw_report_data()
-        incident_types.IncidentTypesProcessor().process(report_df)
-        for col_name in incident_types.IncidentTypesProcessor.col_names:
+        processor.IncidentTypesProcessor().process(report_df)
+        for col_name in processor.IncidentTypesProcessor.col_names:
             for inc_type in report_df[col_name]:
-                if inc_type in incident_types._t_replace_dict.keys():
+                if inc_type in processor._t_replace_dict.keys():
                     self.fail()
 
 
