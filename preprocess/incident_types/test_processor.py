a794b02eae5e24470e6207e5dec8c387099a8b90
Calum Sieppert <sieppertcalum@gmail.com>
2020-05-23 09:57:35 -0600

    NLP-18 Move report data gathering into class

--- preprocess/incident_types/test_processor.py
+++ preprocess/incident_types/test_processor.py
@@ -2,14 +2,14 @@ import unittest
 
 import incident_types.incident_types_d
 from incident_types import processor
-import report_data
+from report_data import ReportData
 
 
 class TestProcessor(unittest.TestCase):
     # TODO: Test that only incident types from the current dropdown are in the columns after pre-processing
     def test_process(self):
         """Ensure replacements are made as expected"""
-        report_df = report_data.get_raw_report_data()
+        report_df = ReportData().get_raw_report_data()
         processor.IncidentTypesProcessor().process(report_df)
         for col_name in processor.IncidentTypesProcessor.col_names:
             for inc_type in report_df[col_name]:
