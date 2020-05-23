30e97d2efe557beb1712a165f1491edda570ad32
Calum Sieppert <sieppertcalum@gmail.com>
2020-05-23 15:36:42 -0600

    NLP-18 Allow for command-line args to script

--- preprocess/README.md
+++ preprocess/README.md
@@ -1,16 +1,20 @@
-# Critical Incident Report Data Scrubber
+# Critical Incident Report Data Pre-processing
 
-This script is used to perform extra data scrubbing on the initial data sent to us by the YW. 
+This package provides pre-processing functionality for the initial data sent to us by the YW.
+
+The `report_data.py` can be run as a script (see below).
 
 The script requires a Python version before 3.7, as 3.7 introduced breaking changes to the used scrubadub library. 
 
 **NOTE:** If the original data is needed, please reach out to the Code the Change YYC organization at [codethechangeyyc@gmail.com](mailto:codethechangeyyc@gmail.com)
 
 USAGE: 
-1. Save the downloaded YW data as `data-sensitive.csv` into this folder.
+1. Save the downloaded YW data as `data-sensitive.csv` into the `data` folder.
 2. Use `pyenv` or another python library to change your Python version to `3.6.10` or older.
 3. Install the requirements using `pip install -r requirements.txt`.
-4. Run the script using `python scrub-form.py`.
+4. Run the script using `python report_data.py`. The script expects 0-2
+arguments. First argument specifies input file path, second specifies output
+file path.
 
 ## Tests
 
--- preprocess/report_data.py
+++ preprocess/report_data.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List, Type
 
 import pandas as pd
@@ -13,15 +14,24 @@ class ReportData:
         DescriptionScrubber,
         IncidentTypesProcessor
     ]
-    file_name_sensitive = "data/data-sensitive.csv"
-    file_name_processed = "data/data-processed.csv"
+    in_file_path: str
+    out_file_path: str
+
+    def __init__(self, in_file_path: str = "data/data-sensitive.csv",
+                 out_file_path: str = "data/data-processed.csv"):
+        """
+        :param in_file_path:
+        :param out_file_path:
+        """
+        self.in_file_path = in_file_path
+        self.out_file_path = out_file_path
 
     def get_raw_report_data(self) -> pd.DataFrame:
         """
         :return: Unprocessed report data.
         """
         # use pandas to get csv description column
-        report_df = pd.read_csv(self.file_name_sensitive)
+        report_df = pd.read_csv(self.in_file_path)
         # Use enum for column access. This works because enum's are iterable and
         # ordered.
         report_df.columns = ColName
@@ -38,8 +48,11 @@ class ReportData:
 
     def create_preprocessed_csv(self):
         """Create new .csv file with scrubbed data."""
-        self.get_report_data().to_csv(self.file_name_processed, index=False)
+        self.get_report_data().to_csv(self.out_file_path, index=False)
 
 
+# Expects 0-2 arguments. First argument specifies input file path, second
+# specifies output file path
 if __name__ == '__main__':
-    ReportData().create_preprocessed_csv()
+    file_names = sys.argv[1:]
+    ReportData(*file_names).create_preprocessed_csv()
