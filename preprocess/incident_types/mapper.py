f4d5167fc1b631c42b0473b8ca8eb31a6d08859a
Calum Sieppert <sieppertcalum@gmail.com>
2020-05-23 19:54:23 -0600

    NLP-18 Separate incident type steps into their own preprocessors

---
+++ preprocess/incident_types/mapper.py
@@ -0,0 +1,35 @@
+from typing import Iterable
+
+import pandas as pd
+
+from incident_types.incident_types_d import replacements
+from preprocessor import Preprocessor
+from report_data_d import ColName
+
+
+def normalize_inc_type(col: pd.Series) -> pd.Series:
+    return col.str.strip().str.capitalize()
+
+
+class Mapper(Preprocessor):
+    """Maps old incident types to their current dropdown counterparts."""
+    col_names: Iterable[str] = {
+        ColName.INC_T1,
+        ColName.INC_T1_OLD,
+        ColName.INC_T2,
+        ColName.INC_T2_OLD,
+    }
+
+    def __init__(self, col_names: Iterable[str] = None):
+        """
+        :param col_names: The names of the incident type columns to process
+        """
+        if col_names is not None:
+            self.col_names = col_names
+
+    def process(self, report_data: pd.DataFrame) -> pd.DataFrame:
+        for col_name in self.col_names:
+            report_data[col_name] = normalize_inc_type(report_data[col_name])
+            report_data[col_name].replace(replacements, inplace=True)
+
+        return report_data
--- preprocess/incident_types/test_mapper.py
+++ preprocess/incident_types/test_mapper.py
@@ -5,12 +5,12 @@ from incident_types.incident_types_d import IncidentType, replacements
 from report_data import ReportData
 
 
-class TestProcessor(unittest.TestCase):
-    def test_process(self):
-        """Ensure replacements are made as expected"""
+class TestMapper(unittest.TestCase):
+    def test_maps(self):
+        """Ensure replacements are made as expected."""
         report_df = ReportData().get_raw_report_data()
         processor.IncidentTypesProcessor().process(report_df)
-        for col_name in processor.IncidentTypesProcessor.col_names:
+        for col_name in processor.Mapper.col_names:
             for inc_type in report_df[col_name].fillna(''):
                 self.assertNotIn(inc_type, replacements.keys())
                 self.assertIn(inc_type, [v.value for v in IncidentType.__members__.values()] + [''])
