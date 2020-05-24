7d86177de777c8b5fbc995e8d2c4cf49808a7bb7
Calum Sieppert <sieppertcalum@gmail.com>
2020-05-24 12:02:53 -0600

    NLP-18 Clean up sys import

--- preprocess/scrub/__init__.py
+++ preprocess/scrub/__init__.py
@@ -1,5 +1,4 @@
-import sys
+from sys import version_info as python_version
 
-python_version = sys.version_info
 if not (python_version.major == 3 and python_version.minor <= 6):
     raise ImportError('This package requires Python version 3.6 or earlier.')
