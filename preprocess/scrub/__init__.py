95671afd68281747482fc73e47b261e3cd566897
Calum Sieppert <sieppertcalum@gmail.com>
2020-05-31 20:27:04 -0600

    NLP-19 Update python version check to a warning instead of an error
    Execution doesn't always need to use scrubadub so we shouldn't stop it when the wrong python version is used.

--- preprocess/scrub/__init__.py
+++ preprocess/scrub/__init__.py
@@ -1,4 +1,5 @@
+import logging
 from sys import version_info as python_version
 
 if not (python_version.major == 3 and python_version.minor <= 6):
-    raise ImportError('This package requires Python version 3.6 or earlier.')
+    logging.warning("This package requires Python version 3.6 or earlier.")
