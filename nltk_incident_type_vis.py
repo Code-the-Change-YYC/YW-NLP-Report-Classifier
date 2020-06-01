75971e904715b48b2e366155059160564acf815f
Calum Sieppert <sieppertcalum@gmail.com>
2020-05-31 20:25:20 -0600

    NLP-19 Example usage of ReportData.get_processed_data()

--- nltk_incident_type_vis.py
+++ nltk_incident_type_vis.py
@@ -4,29 +4,15 @@ import matplotlib
 import nltk
 import pandas as pd
 
-columns = ["DATETIME_INCIDENT", "LOCATION", "LOCATION_DETAIL",
-           "CLIENT_PRIMARY", "CLIENT_SECONDARY", "CHILD_INVOLVED",
-           "NON_CLIENT_INVOLVED", "PROGRAM", "CFS", "EMS", "FIRE",
-           "MISC_SERVICE_FLAG", "DOAP_PACT", "POLICE", "MISC_SERVICE",
-           "INCIDENT_TYPE_1", "INCIDENT_TYPE_2", "INCIDENT_1_OLD",
-           "INCIDENT_TYPE_OTHER", "DESCRIPTION", "RESPONSE_CHILD_WELFARE",
-           "RESPONSE_EVAC", "RESPONSE_FIRST_AID", "RESPONSE_INFECTION_PREVENTION_PROTOCOL",
-           "RESPONSE_MENTAL_ASSESSMENT", "RESPONSE_NALOXONE",
-           "RESPONSE_PERSON_BARRED", "RESPONSE_SAFETY_ASSESSMENT",
-           "RESPONSE_SAFETY_PLANNING", "RESPONSE_OTHER", "RESPONSE_OTHER_DESC",
-           "DATETIME_WRITTEN"]
+from report_data import ReportData
+from report_data_d import ColName
 
-report_data = pd.read_csv("preprocess/data/data-sensitive.csv", na_filter=False)
-report_data.columns = columns
-descriptions = report_data["DESCRIPTION"]
-it1: pd.Series = report_data["INCIDENT_TYPE_1"]
-it1o: pd.Series = report_data["INCIDENT_1_OLD"]
-it2: pd.Series = report_data["INCIDENT_TYPE_2"]
-it2o: pd.Series = report_data["INCIDENT_TYPE_OTHER"]
-c1: pd.Series = it1 + it1o
-c2: pd.Series = it2 + it2o
-c1 = c1.replace(to_replace='', value='UNKNOWN')
-c2 = c2.replace(to_replace='', value='UNKNOWN')
+report_df = ReportData(out_file_path="preprocess/data/data-processed.csv").get_processed_data()
+descriptions = report_df[ColName.DESC]
+it1: pd.Series = report_df[ColName.INC_T1]
+it2: pd.Series = report_df[ColName.INC_T2]
+c1 = it1.replace(to_replace='', value='UNKNOWN')
+c2 = it2.replace(to_replace='', value='UNKNOWN')
 stopwords: List = [*nltk.corpus.stopwords.words('english'), '.', ',']
 tokenized = [nltk.tokenize.word_tokenize(description) for description in descriptions]
 descriptions = [[word.lower() for word in description if word not in stopwords]
