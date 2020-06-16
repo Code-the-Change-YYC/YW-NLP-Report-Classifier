1477dd8ac255ba028a8aec19f3c326b6b265c9f5
Calum Sieppert <sieppertcalum@gmail.com>
2020-06-15 19:11:43 -0600

    Fix and update vectorize definition (#15)
    * NLP-19 Fix vectorize definition and update to use ndarray instead of Series
    
    * NLP-19 Remove unused import

--- vectorize/vectorizer.py
+++ vectorize/vectorizer.py
@@ -1,15 +1,15 @@
 from abc import ABC, abstractmethod
 
-import pandas as pd
+import numpy as np
 
 
 class Vectorizer(ABC):
     @abstractmethod
-    def vectorize(self, strings: pd.Series) -> pd.Series:
+    def vectorize(self, strings: np.ndarray) -> np.ndarray:
         """Transforms strings into their vectorized representation suitable for
         use as a feature vector.
 
-        :param strings: Series of strings to be vectorized.
-        :return: Vectorized representation of strings.
+        :param strings: Strings to be vectorized.
+        :return: Array of vectorized representations of strings.
         """
         ...
