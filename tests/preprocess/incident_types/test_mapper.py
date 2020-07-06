244967d654793b2af690759c863d228718ffc5c0
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-07-06 12:58:46 -0600

    move unit test files to test folder

--- tests/preprocess/incident_types/test_mapper.py
+++ tests/preprocess/incident_types/test_mapper.py
@@ -1,14 +1,19 @@
 import unittest
+import os
 
 from incident_types import processor
 from incident_types.incident_types_d import IncidentType, replacements
 from report_data import ReportData
 
+# use for filepath relative from this file
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 class TestIncTypeMapper(unittest.TestCase):
     def test_maps(self):
         """Ensure replacements are made as expected."""
-        report_df = ReportData(in_file_path='../../../preprocess/data/data-sensitive.csv').get_raw_report_data()
+
+        in_file_path = os.path.join(dir_path, '../../../preprocess/data/data-sensitive.csv')
+        report_df = ReportData(in_file_path=in_file_path).get_raw_report_data()
         processor.IncidentTypesProcessor().process(report_df)
         for col_name in processor.IncTypeMapper.col_names:
             for inc_type in report_df[col_name].fillna(''):
