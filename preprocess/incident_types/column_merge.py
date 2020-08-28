829871e0195086fd35ee8afc65bac177f6a065ec
Calum Sieppert <sieppertcalum@gmail.com>
2020-08-28 09:19:26 -0600

    NLP-43 Process secondary incident type text inputs

--- preprocess/incident_types/column_merge.py
+++ preprocess/incident_types/column_merge.py
@@ -15,11 +15,6 @@ class ColumnMerger(Preprocessor):
         report_data[_ColName.INC_T1] = self.merge(report_data[_ColName.INC_T1], report_data[_ColName.INC_T1_OLD])
         report_data[_ColName.INC_T2] = self.merge(report_data[_ColName.INC_T2], report_data[_ColName.INC_T2_OLD])
 
-        # make blank if matching first incident type
-        report_data[_ColName.INC_T2] = report_data.apply(
-            lambda r: (r[_ColName.INC_T2] if r[_ColName.INC_T1] != r[_ColName.INC_T2] else ''),
-            axis=1)
-
         # remove old columns
         return report_data[[col for col in report_data if col not in [_ColName.INC_T1_OLD, _ColName.INC_T2_OLD]]]
 
--- tests/preprocess/incident_types/test_mapper.py
+++ tests/preprocess/incident_types/test_mapper.py
@@ -1,9 +1,11 @@
 import unittest
 
+from incident_types.column_merge import ColumnMerger
 from incident_types.incident_types_d import IncidentType, replacements
 from incident_types.mapper import IncTypeMapper
 from incident_types.processor import IncidentTypesProcessor
 from report_data import ReportData
+from report_data_d import _ColName
 
 
 class TestIncTypeMapper(unittest.TestCase):
@@ -18,6 +20,28 @@ class TestIncTypeMapper(unittest.TestCase):
             for inc_type in report_df[col_name].fillna(''):
                 self.assertNotIn(inc_type, replacements_keys)
                 self.assertIn(inc_type, incident_type_values)
+
+    def test_preserves_new_secondary_types(self):
+        """
+        Context:
+
+        Secondary incidents of type 'Other' have an optional text input
+        for a more specific description. These values account for the
+        `ColName.INC_T2_OLD` column.
+
+        Test:
+
+        Ensure text inputs are used instead of 'Other' when possible for
+        secondary incident type.
+        """
+
+        report_df = ReportData().get_raw_report_data()
+        rows = (report_df[_ColName.INC_T2] == IncidentType.OTR.value) & (report_df[_ColName.INC_T2_OLD].notnull())
+        text_inputs = report_df[rows][_ColName.INC_T2_OLD]
+        ColumnMerger().process(report_df)
+        for val, text_input in zip(report_df[rows][_ColName.INC_T2], text_inputs):
+            self.assertNotEqual(val, IncidentType.OTR.value)
+            self.assertEqual(val, text_input)
 
 
 if __name__ == '__main__':
