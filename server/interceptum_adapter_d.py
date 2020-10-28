0bec10f5e5f5e84676201f2fbb33df2f978554f0
boyangdong00 <dongboyang00@gmail.com>
2020-10-28 12:03:13 -0600

    Updated immediate response dictionary

--- server/interceptum_adapter_d.py
+++ server/interceptum_adapter_d.py
@@ -25,4 +25,14 @@ interceptum_incident_type_secondary_dict = {
 
 interceptum_immediate_response_dict = {
 
+        "Called Child Welfare" : 1,
+        "Evacution" : 2,
+        "First-aid provided" : 3,
+        "Infecton prevention protocol" : 4,
+        "Mental health assessment" : 5,
+        "Naloxone adminstered" : 10,
+        "Person barred/access restricted": 6,
+        "Safety assessment" : 7,
+        "Safety planning" : 8,
+        "Other" : 9
 }
