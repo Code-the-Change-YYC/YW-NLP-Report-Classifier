4d95fde1e1baac6069d7565b38a887e791a107c2
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2020-10-27 20:10:35 -0600

    create dict stubs

---
+++ server/interceptum_adapter_d.py
@@ -0,0 +1,28 @@
+interceptum_boolean_dict = {
+    True: 1,
+    False: 2
+}
+
+interceptum_services_involved_dict = {
+
+}
+
+interceptum_location_dict = {
+
+}
+
+interceptum_program_form_dict = {
+
+}
+
+interceptum_incident_type_primary_dict = {
+
+}
+
+interceptum_incident_type_secondary_dict = {
+
+}
+
+interceptum_immediate_response_dict = {
+
+}
