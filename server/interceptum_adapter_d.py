8570613495c13437dd1fcc324cb36116009c42a1
Calum Sieppert <sieppertcalum@gmail.com>
2020-10-29 01:26:31 +0000

    NLP-83 Create dict for program field
    This was a great vim race.

--- server/interceptum_adapter_d.py
+++ server/interceptum_adapter_d.py
@@ -11,8 +11,25 @@ interceptum_location_dict = {
 
 }
 
-interceptum_program_form_dict = {
-
+interceptum_program_dict = {
+    'Child Care (Hub)': 1,
+    'Child Support': 10,
+    'Compass': 2,
+    'Counselling and Personal Development': 3,
+    'Croydon (Community Housing)': 4,
+    'DCRT': 5,
+    'Drop-In Child Care': 6,
+    'Employment Resource Center': 7,
+    'Family Access': 8,
+    'Family Resource Network': 14,
+    'Intensive Case Management': 9,
+    'LINC': 11,
+    'Mindful Moments': 20,
+    'Outreach Counselling': 13,
+    'Providence (Community Housing)': 15,
+    'Sheriff King Home': 16,
+    'The Maple (Community Housing)': 19,
+    'Transitional Housing': 12
 }
 
 interceptum_incident_type_primary_dict = {
@@ -24,15 +41,14 @@ interceptum_incident_type_secondary_dict = {
 }
 
 interceptum_immediate_response_dict = {
-
-        "Called Child Welfare" : 1,
-        "Evacution" : 2,
-        "First-aid provided" : 3,
-        "Infecton prevention protocol" : 4,
-        "Mental health assessment" : 5,
-        "Naloxone adminstered" : 10,
-        "Person barred/access restricted": 6,
-        "Safety assessment" : 7,
-        "Safety planning" : 8,
-        "Other" : 9
+    "Called Child Welfare": 1,
+    "Evacution": 2,
+    "First-aid provided": 3,
+    "Infecton prevention protocol": 4,
+    "Mental health assessment": 5,
+    "Naloxone adminstered": 10,
+    "Person barred/access restricted": 6,
+    "Safety assessment": 7,
+    "Safety planning": 8,
+    "Other": 9
 }
