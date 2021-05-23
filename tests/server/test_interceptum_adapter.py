8f02e1109205b293f065f155e847f14bd4c56880
boyangdong00 <dongboyang00@gmail.com>
2021-05-22 22:11:27 -0600

    finish xml to form
    - implement xml_to_form_values
    - tested with unittest

--- tests/server/test_interceptum_adapter.py
+++ tests/server/test_interceptum_adapter.py
@@ -8,7 +8,9 @@ with open('tests/server/interceptum-response.xml', 'r') as xml_file:
 class TestXmlToFormValues(TestCase):
 
     def test_mapping(self):
+
         result = InterceptumAdapter.xml_to_form_values(xml)
+
         expected = {
             "description": "Example description from form field.",
             "client_primary": "AB",
@@ -19,16 +21,16 @@ class TestXmlToFormValues(TestCase):
             "services_involved_other": "police",
             "primary_staff_first_name": "John",
             "primary_staff_last_name": "Doe",
-            "occurrence_time": "2008-09-15T15:53:00+05:00",
-            "incident_type_primary": "Child abandonment",
+            "occurrence_time": "2008-09-15 04:53:00",
+            "incident_type_primary": "child abandonment",
             "incident_type_secondary": "injury",
             "child_involved": True,
             "non_client_involved": False,
             "program": "compass",
-            "immediate_response": ["evacuation", "mental health assessment"],
+            "immediate_response": ["evacution", "mental health assessment"],
             "staff_name": "John man",
             "program_supervisor_reviewer_name": "another john",
-            "completion_date": "2008-09-15T15:53:00+05:00"
+            "completion_date": "2008-09-15 04:53:00"
         }
         self.assertDictEqual(result, expected)
 
