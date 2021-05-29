c2349b95888a92ca39333f3a1312f15e2407318c
Jofred Cayabyab <jofred.cayabyab1@ucalgary.ca>
2021-05-28 19:13:06 -0600

    update title and icon

--- client/src/App.js
+++ client/src/App.js
@@ -206,7 +206,7 @@ function App() {
       ></ModalBox>
       <img src={logo} alt="YW logo"></img>
       <h1>Critical Incident Report Form</h1>
-      <LastUpdated>Last updated: May 10, 2021</LastUpdated>
+      <LastUpdated>Last updated: May 28, 2021</LastUpdated>
       <FormRow>
         <label>Description of Incident *</label>
         <Textarea
